512aea9d52bc180762e9fa7817b5b2946f77ecb8
Sohini Palit <sohiniplt@gmail.com>
2019-02-27 17:17:51 -0500

    Create about.js

---
+++ components/about.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div className="background-color">
+        <div><h1>About page</h1></div>
+        <img
+          src="https://i.ytimg.com/vi/kgVDoVLEe2c/maxresdefault.jpg"
+          alt="UnderConstruction"
+          className="style-img"
+        /> 
+      </div>
+    )
+  }
+}
+
+export default About;
